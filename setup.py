463618fc50cd20a4f03b48f4425f31c5d13f6f4f
Anton K <77491231+macmorn@users.noreply.github.com>
2022-11-13 14:33:51 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='os2d',
+version='1.0',
+description='First installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['os2d'],
+setup_requires=['wheel']
+)
